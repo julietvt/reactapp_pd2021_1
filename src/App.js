496b03d7339a43228e8d5ba574bfe83f6a0bec82
julietvt <freshitpt@gmail.com>
2021-10-16 12:47:22 +0300

    add styles for hello component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello';
 
+// Можем вернуть только 1 элемент!!!
+// <> ... </> фрагмент
+// <>...</> сокращение от <React.Fragment></React.Fragment>
 function App() {
   return (
     <>
--- src/components/Hello/index.jsx
+++ src/components/Hello/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./Hello.module.scss"
 
 // props это пропсы , входящие параментр 
 // при отрисовке переменная в {}
 const Hello = props => {
     const {name, surname} = props;
-    return <div>Hello, {name} {surname}!!!</div>;
+    return <div className={styles.wrapper_hello}>Hello, {name} {surname}!!!</div>;
 };
 
 export default Hello;
