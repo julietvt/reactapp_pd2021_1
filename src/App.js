ba19993324d83b3fa89be7485cf4d27e75b81a58
julietvt <freshitpt@gmail.com>
2021-10-16 12:21:41 +0300

    composition

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import Hello from './components/Hello';
 
 function App() {
-  return <Hello name={'John'} surname={'Fox'} />;
+  return (
+    <>
+      <Hello name={'John'} surname={'Fox'} />
+      <Hello name={'Vasya'} surname={'Ivanov'} />
+      <Hello name={'Maria'} surname={'Smith'} />
+    </>
+  );
 }
 
 export default App;
